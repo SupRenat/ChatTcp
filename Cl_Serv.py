d697bbad6aadeea73ec6e7c060aee5c1d5ce39c3
SupRenat <32498476+SupRenat@users.noreply.github.com>
2017-10-12 14:00:15 +0700

    Update Cl_Serv.py
    Fix some bugs with connection

--- Cl_Serv.py
+++ Cl_Serv.py
@@ -1,17 +1,40 @@
 import socket
 from tkinter import *
+import json
+import random
+import generator
+
+count = random.randint(3,5)
+tempMin = 23
+tempMax = 27
+pressMin = 742
+pressMax = 750
+wetMin = 70
+wetMax=80
+radMin = 0
+radMax = 0.5
+windStrMin = 0
+windStrMax = 3
+#values = {'tempMin':23,'tempMax':27,'pressMin':742, 'pressMax':750,'wetMin':70,'wetMax':80,'radMin':0,'radMax':0.5,'windStrMin':0, 'windStrMax':3}
+weird_json = generator.Generate(count,tempMin,tempMax,pressMin,pressMax,wetMin,wetMax,radMin,radMax,windStrMin,windStrMax)
+print(weird_json)
+send = json.dumps(weird_json)
 
 tk=Tk()
 
-s = socket.socket()
-host =socket.gethostbyname(socket.gethostname())
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect(("8.8.8.8", 80))
+host = s.getsockname()[0]
+#host =socket.gethostbyname(socket.gethostname())
+s.close()
 print(host)
 #host = "0.0.0.0"
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 port = 11719
 s.bind((host, port))
-
 s.listen(5)
-c = None
+
 
 #интерфейс
 text=StringVar()
@@ -37,22 +60,11 @@ log.pack(side='top', fill='both',expand='true')
 def loopproc():
     log.see(END)
     s.setblocking(False)
+    c = None
     try:
-        while True:
-            if c is None:
-                # Halts
-                print('[Waiting for connection...]')
-                c, addr = s.accept()
-                print('Got connection from', addr)
-            else:
-                # Halts
-                print
-                '[Waiting for response...]'
-                print
-                message =c.recv(1024)
-                log.insert(END, message.decode("utf-8") + '\n')
-                #q = raw_input("Enter something to this client: ")
-                #c.send(q)
+        c, addr = s.accept()
+        message = c.recv(128)
+        log.insert(END, message.decode("utf-8") + '\n')
     except:
         tk.after(1, loopproc)
         return
@@ -60,8 +72,9 @@ def loopproc():
     return
 
 def sendproc(event):
+    WtF=0
     i=0
-    addr=[("127.0.0.2", 11719),("192.168.56.1",11719),("127.0.0.1",11719)]
+    addr=[("192.168.43.179", 11719),("192.168.56.1",11719),("127.0.0.1",11719)]
     addrL = len(addr)
     for i in  range (addrL):
        try:
@@ -69,15 +82,21 @@ def sendproc(event):
             s.settimeout(2)
             s.connect(addr[i])
             s.settimeout(None)
-            sData = name.get()+':'+text.get()
-            s.send(sData.encode("utf-8"))#тут меняем ip-шники
-            log.insert(END, sData + '\n')
+            sData = name.get() + ':' + text.get() #должно стоять в пробеле ниже, добавил сюда чтоб можно было проверить и записать этот коммит для исправления в дальнейшем
+            s.send(send.encode())  # тут меняем ip-шники  sData.encode("utf-8")
+            log.insert(END, send + '\n')
             text.set('')
             s.close()
             break
        except:
-           print("error %s - is not available" % i)
-
+            WtF += 1
+            s.close()
+            print("error %s - is not available" % i)
+    if (WtF == addrL):
+        print("in file")
+        f = open('data.txt', 'w')
+        f.write(send + '\n')
+        f.close()   
 
 
 msg.bind('<Return>',sendproc)
